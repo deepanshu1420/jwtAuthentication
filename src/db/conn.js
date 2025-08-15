ef63ea6040bb63fe6bb28809383775b5a0f6733d
Deepanshu Sharma <deepanshu3674.be20@chitkara.edu.in>
2025-08-15 06:53:52 +0530

    db fixed

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,26 @@
-const mongoose = require("mongoose");
-async function connectDatabase(){
-    try{
-        const res = await mongoose.connect(`mongodb+srv://deepanshu:${process.env.DB_PASSWORD}@cluster0.vstxtwv.mongodb.net/${process.env.DATABASE_HOST}`);
-        console.log("Database connection is successful ....");
-    }catch(e){
-        console.log(e);
-    }
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.flcmr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Connected to MongoDB");
+  } catch (err) {
+    console.error("❌ Connection failed:", err);
+  } finally {
+    await client.close();
+  }
 }
-connectDatabase();
+
+connectDB();
